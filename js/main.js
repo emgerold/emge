19fa92319e289905d3bcd74aa4be344517cf1d66
Moritz Gerold <moritz.gerold@screenlight.ch>
2014-11-13 23:49:16 +0100

    immer a bissle besser

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
-  var $links = $('[data-link]');
+  var $links = $('[data-link]'),
+  		counter = 0;
 
   $('.wrapper').one('click', function(){
     $(this).removeClass('start-animation');
 
     $links.on('click', function(){
-      // $links.removeClass('active-content');
       $(this).toggleClass('active-content');
+      if ($(this).hasClass('active-content')) counter++;
+      else counter--;
+      if (counter === 4) {
+      	setTimeout(function(){
+      		$('.wrapper').addClass('final-state');
+      		$('[data-link="e1"], [data-link="e2"]').removeClass('active-content');
+      		counter -= 2;
+      	}, 1500);
+      }
+
     });
   });
 });
