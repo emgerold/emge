2ac0ce287628e0d1ae6d571637ea15fd3a0c17d0
Moritz Gerold <moritz.gerold@screenlight.ch>
2014-11-30 23:54:54 +0100

    hoch risiko spiel sei dank

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 $(document).ready(function(){
+
+  $('.square-container').each(function(){
+    // setScrollPosition($(this));
+  });
 });
+
+function setScrollPosition($el, min,max) {
+  var lowerBound = min ? min : 0,
+      upperBound = max ? max : 700,
+      scroll = randomIntFromInterval(lowerBound, upperBound);
+  $el.scrollTo(Math.floor(scroll));
+}
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
